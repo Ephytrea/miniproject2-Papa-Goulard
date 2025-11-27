add7c925e8341df984f5ce83f691ff0920a9532d
Ephytrea <Noan.Goulard@etu.unige.ch>
2025-11-27 11:26:08 +0100

    Add the second file before starting.

---
+++ architecture.py
@@ -0,0 +1,26 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CausalSelfAttention(nn.Module):
+    def __init__():
+
+    def forward(self, x):
+
+
+class MLP(nn.Module):
+    def __init__():
+
+    def forward(self, x):
+
+
+class Block(nn.Module):
+    def __init__():
+
+    def forward(self, x):
+
+class completeModel(nn.Module):
+    def __init__():
+
+    def forward(self, x):
